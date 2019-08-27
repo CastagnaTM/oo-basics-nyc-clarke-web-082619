2ecf07706e7b3715a8c745eb75ef76400f29f1f6
castagnaTM <castagna.tm@gmail.com>
2019-08-27 15:56:36 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,21 @@
+class Book
+
+    def initialize(title)
+        @title = title
+    end
+
+    attr_accessor :title, :author, :genre
+    attr_reader :page_count
+
+    def page_count=(number)
+        @page_count = number
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+
+end
 
 
